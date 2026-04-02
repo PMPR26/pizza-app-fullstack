75a01e7561b7b131fbf89d35485407c09be284f6
Patricia Pinto <prpatriciam@gmail.com>
2026-04-02 18:44:50 -0400

    feat(menu): display pizza ingredients in menu cards

--- frontend/src/components/PizzaCard.vue
+++ frontend/src/components/PizzaCard.vue
@@ -1,11 +1,37 @@
 <template>
   <article class="pizza-card">
     <img :src="pizza.image" :alt="pizza.name" class="pizza-card__image" />
+
     <div class="pizza-card__content">
       <h3 class="pizza-card__title">{{ pizza.name }}</h3>
-      <p class="pizza-card__description">{{ pizza.description }}</p>
-      <p class="pizza-card__price">Bs {{ Number(pizza.price).toFixed(2) }}</p>
-      <button @click="emit('add', pizza)" class="pizza-card__button">
+
+      <p class="pizza-card__description">
+        {{ pizza.description }}
+      </p>
+
+      <div
+        v-if="pizza.ingredients?.length"
+        class="pizza-card__ingredients"
+      >
+        <strong>Ingredientes:</strong>
+        <ul>
+          <li
+            v-for="ingredient in pizza.ingredients"
+            :key="ingredient.id"
+          >
+            {{ ingredient.name }}
+          </li>
+        </ul>
+      </div>
+
+      <p class="pizza-card__price">
+        Bs {{ Number(pizza.price).toFixed(2) }}
+      </p>
+
+      <button
+        @click="emit('add', pizza)"
+        class="pizza-card__button"
+      >
         Agregar al carrito
       </button>
     </div>
@@ -16,7 +42,7 @@
 import type { Pizza } from '@/types'
 import '@/assets/styles/pizza-card.css'
 
-const props = defineProps<{
+defineProps<{
   pizza: Pizza
 }>()
 
@@ -24,4 +50,3 @@ const emit = defineEmits<{
   add: [pizza: Pizza]
 }>()
 </script>
-
